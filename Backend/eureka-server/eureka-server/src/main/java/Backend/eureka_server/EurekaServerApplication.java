f55b9253aeba1da0f0a87a58b7bd61313fe7a434
TamaraCerna <166471056+TamaraCerna@users.noreply.github.com>
2026-01-02 10:46:15 -0300

    cambio de api-gateway

--- Backend/eureka-server/eureka-server/src/main/java/Backend/eureka_server/EurekaServerApplication.java
+++ Backend/eureka-server/eureka-server/src/main/java/Backend/eureka_server/EurekaServerApplication.java
@@ -1,12 +1,16 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
-@SpringBootApplication
+@SpringBootApplication(
+        exclude = {
+                org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration.class
+        }
+)
 public class EurekaServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(EurekaServerApplication.class, args);
     }
 }
-
