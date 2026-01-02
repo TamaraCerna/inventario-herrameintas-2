e10e520fb88b4619e132ee30f20243f7f256551c
TamaraCerna <166471056+TamaraCerna@users.noreply.github.com>
2026-01-02 11:46:51 -0300

    modificacion de config data

--- Backend/eureka-server/eureka-server/src/main/java/Backend/eureka_server/EurekaServerApplication.java
+++ Backend/eureka-server/eureka-server/src/main/java/Backend/eureka_server/EurekaServerApplication.java
@@ -1,15 +1,13 @@
+package Backend.eureka_server;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@SpringBootApplication
 @EnableEurekaServer
-@SpringBootApplication(
-        exclude = {
-                org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration.class
-        }
-)
 public class EurekaServerApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(EurekaServerApplication.class, args);
     }
